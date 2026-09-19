9636a598c8f3f1f8d98a3f78b1c76bd2ca54f478
GlitchSlayed <60859364+GlitchSlayed@users.noreply.github.com>
2026-09-19 14:28:41 -0400

    Add files via upload

---
+++ src/client/java/com/apotheosish/client/ApotheosIshClient.java
@@ -0,0 +1,10 @@
+package com.apotheosish.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ApotheosIshClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
